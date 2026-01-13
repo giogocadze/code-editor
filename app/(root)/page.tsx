0b0a7786ed4651499b045e18a5a6c92a7b6f25e2
giogocadze <giogocadze5@gmail.com>
2026-01-13 22:05:43 +0400

    chore: restructure app routes using route groups

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      <div className="max-w-450 mx-auto p-4">
+        <div>header</div>
+
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          editor and preview
+        </div>
+      </div>
+    </div>
+  );
+}
