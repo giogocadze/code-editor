1e60ef8ac947e95448356f02a377f6fdc6e5e67b
giogocadze <giogocadze5@gmail.com>
2026-01-14 19:55:28 +0400

    feat: establish editor state foundation

--- app/store/useCreateStore.ts
+++ app/store/useCreateStore.ts
@@ -7,7 +7,7 @@ const getInitialState = () => {
   // if server return default value
   if (typeof window == "undefined") {
     return {
-      languange: "javascript",
+      language: "javascript",
       fontSize: 16,
       theme: "vs-dark",
     };
@@ -15,15 +15,56 @@ const getInitialState = () => {
 
   const savedLanguage = localStorage.getItem("editor-language") || "javascript";
   const savedTheme = localStorage.getItem("editor-theme") || "vs-dark";
-  const savedFontSize = localStorage.getItem("editor-font-size") || 16
+  const savedFontSize = localStorage.getItem("editor-font-size") || 16;
   return {
-    langaunge: savedLanguage,
+    language: savedLanguage,
     theme: savedTheme,
-    fontSize : Number(savedFontSize)
+    fontSize: Number(savedFontSize),
   };
 };
 export const useCodeEditorState = create<CodeEditorState>((set, get) => {
   const initialState = getInitialState();
 
-  return {};
+  return {
+    ...initialState,
+    output: "",
+    isRunning: false,
+    error: null,
+    editor: null,
+    executionResult: null,
+
+    getCode: () => get().editor?.getValue() || "",
+
+    setEditor: (editor: Monaco) => {
+      const savedCode = localStorage.getItem(`editor-code-${get().language}`);
+
+      if (savedCode) editor.setValue(savedCode);
+      set({ editor });
+    },
+
+    setTheme: (theme: string) => {
+      localStorage.setItem("editor-theme", theme);
+      set({ theme });
+    },
+
+    setFontSize: (fontSize: number) => {
+      localStorage.setItem("editor-font-size", fontSize.toString());
+      set({ fontSize });
+    },
+
+    setLanguage: (language: string) => {
+      const currentCode = get().editor?.getValue();
+      if (currentCode) {
+        localStorage.setItem(`editor-code-${get().language}`, currentCode);
+      }
+
+      localStorage.setItem("editor-language", language);
+
+      set({
+        language,
+        output: "",
+        error: null,
+      });
+    },
+  };
 });
