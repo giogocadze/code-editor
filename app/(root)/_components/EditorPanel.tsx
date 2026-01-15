1e2dbab8192ba51f7cb20d269736ad3607c3ce2b
giogocadze <giogocadze5@gmail.com>
2026-01-15 17:27:55 +0400

    feat: scaffold editor panel and wire editor state

--- app/(root)/_components/EditorPanel.tsx
+++ app/(root)/_components/EditorPanel.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+"use client"
+import { useCodeEditorStore } from '@/app/store/useCreateStore'
+import { useEffect, useState } from 'react'
+import { LANGUAGE_CONFIG } from '../_constants'
 
 const EditorPanel = () => {
+
+  const [isShareDialogOpen, setIsShareDialogOpen] = useState(false)
+
+  const { language, theme, fontSize, editor, setEditor, setFontSize } = useCodeEditorStore()
+
+  useEffect(() => {
+    const savedCode = localStorage.getItem(`editor-code${language}`)
+    const newCode = savedCode || LANGUAGE_CONFIG[language].defaultCode
+    if (editor) editor.setValue(newCode)
+  }, [language, editor])
+  useEffect(() => {
+    const savedFontSize = localStorage.getItem("editor-font-size")
+    if (savedFontSize) setFontSize(parseInt(savedFontSize))
+  })
+
+  const handleRefresh = () => { }
+  const handleEditorChange = () => { }
+  const handleFontSizeChange = () => {}
   return (
-    <div>
+    <div className='relative' >
       Editor Panel
     </div>
   )
