6a423a68e6c0995a64d740b8c5fd3a0f34598442
giogocadze <giogocadze5@gmail.com>
2026-01-15 23:31:22 +0400

    feat: started integrating code execution via Piston API

--- app/store/useCreateStore.ts
+++ app/store/useCreateStore.ts
@@ -2,6 +2,7 @@ import { create } from "zustand";
 import { LANGUAGE_CONFIG } from "../(root)/_constants";
 import { Monaco } from "@monaco-editor/react";
 import { CodeEditorState } from "@/app/types";
+import { error } from "console";
 
 const getInitialState = (): Pick<
   CodeEditorState,
@@ -67,7 +68,43 @@ export const useCodeEditorStore = create<CodeEditorState>((set, get) => {
     },
 
     runCode: async () => {
-    
+      const { language, getCode } = get();
+      const code = getCode();
+
+      if (!code) {
+        set({ error: "please return some function" });
+        return;
+      }
+
+      set({ isRunning: true, error: null, output: "" });
+      try {
+        const runtime = LANGUAGE_CONFIG[language].pistonRuntime;
+
+        const response = await fetch("https://emkc.org/v2/piston/execute", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            language: runtime.language,
+            version: runtime.version,
+            files: [
+              {
+                content: code,
+              },
+            ],
+          }),
+        });
+
+        const data = await response.json();
+        console.log("data back from piston", data);
+
+        if (data.message) {
+          set({ error: data.message, executionResult: { code, output : "", error : data.message} });
+          return
+        }
+        
+      } catch (error) {}
     },
   };
 });
