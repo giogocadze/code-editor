c2af3d3f122db58ca9481d9e577329b436fc1c3d
giogocadze <giogocadze5@gmail.com>
2026-01-16 23:29:43 +0400

    feat: add snippet listing and star queries

--- convex/snippets.ts
+++ convex/snippets.ts
@@ -1,5 +1,5 @@
 import { v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 export const createSnippet = mutation({
   args: {
@@ -11,7 +11,7 @@ export const createSnippet = mutation({
     const identy = await ctx.auth.getUserIdentity();
     if (!identy) throw new Error("user not authenthicated");
 
-    identy.na
+    identy.na;
 
     const user = await ctx.db
       .query("users")
@@ -21,15 +21,56 @@ export const createSnippet = mutation({
 
     if (!user) throw new Error("User not found");
 
-    const snippetId = await ctx.db.insert("snippets" , {
-        userId: identy.subject,
-        userName : user.name, 
-        title : args.title,
-        language : args.language,
-        code : args.code,
+    const snippetId = await ctx.db.insert("snippets", {
+      userId: identy.subject,
+      userName: user.name,
+      title: args.title,
+      language: args.language,
+      code: args.code,
+    });
 
-    })
+    return snippetId;
+  },
+});
 
-    return snippetId
+export const getSnippets = query({
+  handler: async (ctx) => {
+    const snippets = await ctx.db.query("snippets").order("desc").collect();
+    return snippets;
+  },
+});
+
+export const isSnipetStart = query({
+  args: { snippetId: v.id("snippets") },
+
+  handler: async (ctx, args) => {
+    const identy = await ctx.auth.getUserIdentity();
+
+    if (!identy) return false;
+
+    const star = ctx.db
+      .query("stars")
+      .withIndex("by_user_id_and_snippet_id")
+      .filter(
+        (q) =>
+          q.eq(q.field("userId"), identy.subject) &&
+          q.eq(q.field("snippetId"), args.snippetId)
+      )
+      .first();
+
+    return !!star;
+  },
+});
+
+export const getSnippetStarCount = query({
+  args: { snippetId: v.id("snippets") },
+  handler: async (ctx, args) => {
+    const stars = await ctx.db
+      .query("stars")
+      .withIndex("by_snippet_id")
+      .filter((q) => q.eq(q.field("snippetId"), args.snippetId))
+      .collect();
+
+    return stars.length;
   },
 });
