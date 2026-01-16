986b11be4db36eafbbff773e71fb67a5f5d67e0f
giogocadze <giogocadze5@gmail.com>
2026-01-16 23:45:56 +0400

    feat: add skeleton loading state for snippets page

---
+++ app/snippets/_components/NavigationHeader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavigationHeader = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NavigationHeader
--- app/snippets/page.tsx
+++ app/snippets/page.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+"use client"
+
+import { api } from "@/convex/_generated/api"
+import { useQuery } from "convex/react"
+import { useState } from "react"
+import NavigationHeader from "./_components/NavigationHeader"
+import SnippetsPageSkeleton from "./_components/SnippetsPageSkeleton"
 
 const page = () => {
-  return (
-    <div>
-      snippets page
-    </div>
-  )
+
+    const snippets = useQuery(api.snippets.getSnippets)
+    const [searchQuery, setSearchQuery] = useState("")
+    const [selectedLanguage, setSelectedLanguage] = useState<string | null>(null);
+    const [view, setView] = useState<"grid" | "list">("grid");
+
+    if (snippets === undefined) {
+        return (
+            <div className="min-h-screen" >
+                <NavigationHeader />
+                <SnippetsPageSkeleton />
+            </div>
+        )
+    }
+    return (
+        <div>
+            snippets page
+        </div>
+    )
 }
 
 export default page
