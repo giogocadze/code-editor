3b127addb8c449617e81b57957d3780cb4f50e00
giogocadze <giogocadze5@gmail.com>
2026-01-20 10:47:20 +0400

    feat: added loading skeleton for snippet page

--- app/snippets/[id]/page.tsx
+++ app/snippets/[id]/page.tsx
@@ -1,9 +1,19 @@
+"use client"
 
+import { api } from "@/convex/_generated/api"
+import { Id } from "@/convex/_generated/dataModel"
+import { useQuery } from "convex/react"
+import { useParams } from "next/navigation"
+import SnippetLoadingSkeleton from "./_components/SnippetLoadingSkeleton"
 
 function page() {
+    const snippetId = useParams().id
+    const snippet = useQuery(api.snippets.getSnippetById, {snippetId: snippetId as Id<"snippets">})
+
+    if (snippet == undefined) return <SnippetLoadingSkeleton />
     return (
         <div>
-            
+
         </div>
     )
 }
