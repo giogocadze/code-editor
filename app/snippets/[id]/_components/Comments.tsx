91e81f37419bfb4b4498f84d6abdf8d554b3d5f7
giogocadze <giogocadze5@gmail.com>
2026-01-20 20:20:06 +0400

    feat(comments): implement add comment flow with Convex mutation

--- app/snippets/[id]/_components/Comments.tsx
+++ app/snippets/[id]/_components/Comments.tsx
@@ -1,7 +1,44 @@
+import { api } from '@/convex/_generated/api';
 import { Id } from '@/convex/_generated/dataModel'
+import { useUser } from '@clerk/nextjs';
+import { useMutation, useQuery } from 'convex/react';
+import { useState } from 'react';
+import toast from 'react-hot-toast';
 
 
 function Comments({ snippetId }: { snippetId: Id<"snippets"> }) {
+  const { user } = useUser();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [deletinCommentId, setDeletingCommentId] = useState<string | null>(null);
+
+  const comments = useQuery(api.snippets.getComments, { snippetId }) || [];
+  const addComment = useMutation(api.snippets.addComment);
+  // const deleteComment = useMutation(api.snippets.deleteComment);
+  const handleSubmitComment = async (content: string) => {
+    setIsSubmitting(true);
+
+    try {
+      await addComment({ snippetId, content });
+    } catch (error) {
+      console.log("Error adding comment:", error);
+      toast.error("Something went wrong");
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  const handleDeleteComment = async (commentId: Id<"snippetComments">) => {
+    setDeletingCommentId(commentId);
+
+    try {
+      // await deleteComment({ commentId });
+    } catch (error) {
+      console.log("Error deleting comment:", error);
+      toast.error("Something went wrong");
+    } finally {
+      setDeletingCommentId(null);
+    }
+  };
   return (
     <div>
       Comments
