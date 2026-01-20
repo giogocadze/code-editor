866368550f7334cd2d088010faa2f977fee5f35a
giogocadze <giogocadze5@gmail.com>
2026-01-20 11:51:37 +0400

    feat: added comments section to snippet detail page

---
+++ app/snippets/[id]/_components/Comments.tsx
@@ -0,0 +1,12 @@
+import { Id } from '@/convex/_generated/dataModel'
+
+
+function Comments({ snippetId }: { snippetId: Id<"snippets"> }) {
+  return (
+    <div>
+      Comments
+    </div>
+  )
+}
+
+export default Comments
